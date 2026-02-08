5937635b283b168fcdd95cefededd77267d910f2
Timur1511 <Timur15112008@mail.ru>
2026-02-09 00:41:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
 //"use strict"
 
 const header = document.querySelector("#header")
-const head = document.querySelector("head")
-const aside = document.querySelector(".sidebar")
-const copyright = document.querySelector(".copyright")
+const aside = document.querySelector("#sidebar")
+const copyright = document.querySelector("#copyright")
 
 //head.innerHTML = `
 
 //`
 header.innerHTML = `
     <li class="headerElement">
-        <a href="">Главная</a>
+        <a href="index.html">Главная</a>
     </li>
     <li class="headerElement">
         <a href="">Инструменты</a>
     </li>
 `
 
-/*aside.innerHTML = `
-
-`
-
 copyright.innerHTML = `
     Copyright © 2026 Gataullin T. R. (Timur1511)
 `
-*/
